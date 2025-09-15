4e0402e5a5261a20e877203979432ed9d90c848f
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-09-15 15:59:09 -0700

    Time: 6 ms (46.37%) | Memory: 56 MB (85.27%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.js
@@ -0,0 +1,28 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    const rows = Array.from({length:9},()=> new Set());
+    const cols = Array.from({length:9},()=> new Set());
+    const boxes = Array.from({length:9},()=> new Set());
+
+    for(let r=0;r<9;r++){
+        for(let c=0;c<9;c++){
+            const val = board[r][c];
+
+            if(val==='.') continue;
+
+            if(rows[r].has(val)) return false;
+            rows[r].add(val);
+
+            if(cols[c].has(val)) return false;
+            cols[c].add(val);
+
+            const boxid = (Math.floor(r/3)*3) + Math.floor(c/3);
+            if(boxes[boxid].has(val)) return false;
+            boxes[boxid].add(val);
+        }
+    }
+    return true;
+};
