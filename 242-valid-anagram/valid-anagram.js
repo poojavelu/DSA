8f4095bfb4abc10974fb3baf455b2b8054eb5805
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-09-15 15:37:50 -0700

    Time: 9 ms (86.09%) | Memory: 55.2 MB (94.65%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = {};
+
+    for (let i = 0; i < s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) + 1;
+        count[t[i]] = (count[t[i]] || 0) - 1;
+    }
+
+    // Check if all counts are zero
+    return Object.values(count).every(val => val === 0);
+};
