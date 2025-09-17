1ab571dca974e807da88773552652c0de8d9894d
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-09-17 12:28:20 -0700

    Time: 3 ms (62.22%) | Memory: 66.4 MB (8.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // let l=0,r=1;
-    // let maxP=0;
-    // while(r<prices.length){
-    //     if(prices[r]>prices[l]){
-    //         maxP=Math.max(maxP,prices[r]-prices[l])
-    //     }else{
-    //         l=r;
-    //     }
-    //     r++
-    // }
-    // return maxP;
-
-        let maxP = 0;
-        let minBuy = prices[0];
-
-        for (let sell of prices) {
-            maxP = Math.max(maxP, sell - minBuy);
-            minBuy = Math.min(minBuy, sell);
+    let l=0,r=1;
+    let maxP=0;
+    while(r<prices.length){
+        if(prices[r]>prices[l]){
+            maxP=Math.max(maxP,prices[r]-prices[l])
+        }else{
+            l=r;
         }
-        return maxP;
+        r++
+    }
+    return maxP;
+
+        // let maxP = 0;
+        // let minBuy = prices[0];
+
+        // for (let sell of prices) {
+        //     maxP = Math.max(maxP, sell - minBuy);
+        //     minBuy = Math.min(minBuy, sell);
+        // }
+        // return maxP;
 };
