7fc9cb1b4fa63384245c59186787113e85aca738
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-09-18 10:26:08 -0700

    Time: 3 ms (58.36%) | Memory: 58.5 MB (7.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -10,8 +10,12 @@ var isValid = function(s) {
         '}':'{'
     }
     for(let c of s){
-        if(stack.length>0 && stack[stack.length-1]===closeToOpen[c]){
-            stack.pop();
+        if(closeToOpen[c]){
+            if(stack.length>0 && stack[stack.length-1]===closeToOpen[c]){
+                stack.pop();
+            }else{
+                return false;
+            }           
         }else{
             stack.push(c);
         }
