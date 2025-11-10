51130990072cbd123b37ca0e72f62b68cb3f8ee7
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-10 10:36:51 -0800

    Time: 20 ms (52.42%) | Memory: 73.9 MB (46.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,9 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const newSet= new Set(nums);
-    if(nums.length===newSet.size){
-        return false;
+    let set = new Set()
+    for (let num of nums) {
+        if (set.has(num)) return true
+        set.add(num)
     }
-    return true;
+    return false
 };
