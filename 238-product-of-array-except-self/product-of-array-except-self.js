36b70ddbad1b56b85a542c99ebfc41c165d42d5c
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-10 15:26:20 -0800

    Time: 5 ms (74.32%) | Memory: 73.8 MB (17.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let n=nums.length;
-    let output=new Array(n).fill(1);
+    const n= nums.length;
+    const output=new Array(n).fill(1);
 
-    let  prefix=1;
-    for(let i=0;i<n;i++){
+    let prefix=1;
+    for (let i = 0; i < n; i++) {
         output[i]=prefix;
         prefix*=nums[i];
     }
 
     let suffix=1;
-    for(let i=n-1;i>=0;i--){
+    for (let i = n-1; i >=0; i--) {
         output[i]*=suffix;
         suffix*=nums[i];
     }
 
-    return output
+    return output;
 };
