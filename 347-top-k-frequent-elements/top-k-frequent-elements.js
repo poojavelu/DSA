88f8599b63ad06b1d375bdeeb545a0ff0c5aeff9
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-10 15:19:01 -0800

    Time: 10 ms (73.63%) | Memory: 59.3 MB (62.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -3,12 +3,13 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {       
-    const map={};
-    for(let num of nums){
-        map[num]=(map[num]||0)+1;
+var topKFrequent = function(nums, k) {
+    const res={};
+    for(let i=0;i<nums.length;i++){
+        res[nums[i]] = (res[nums[i]] || 0) + 1;
     }
-    const pairs = Object.entries(map);
-    pairs.sort((a, b) => b[1] - a[1]);
-    return pairs.slice(0, k).map(pair => Number(pair[0]));   
+    const pairs=Object.entries(res);
+    pairs.sort((a,b)=>b[1]-a[1]);
+
+    return pairs.slice(0,k).map(pair=>Number(pair[0]));
 };
