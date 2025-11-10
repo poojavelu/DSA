2ec83714345c3534cdffab381fff39d5ceb3028a
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-10 15:54:29 -0800

    Time: 3 ms (85.56%) | Memory: 55.8 MB (90.79%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,24 +3,26 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-        const rows= Array.from({length:9},()=>new Set());
-        const cols= Array.from({length:9},()=>new Set());
-        const boxes= Array.from({length:9},()=>new Set());
-        for(let r=0;r<9;r++){
-            for(let c=0;c<9;c++){
-                const val=board[r][c];
-                if(val==='.') continue;
+    
+    const rows= Array.from({length:9},()=>new Set());
+    const cols= Array.from({length:9},()=>new Set());
+    const boxes= Array.from({length:9},()=>new Set());
+    for(let r=0; r<9; r++) {
+        for(let c=0; c<9; c++){
+            let value=board[r][c];
+            if(value==='.') continue;
 
-                if(rows[r].has(val)) return false;
-                rows[r].add(val);
+            if(rows[r].has(value)) return false;
+            rows[r].add(value);
 
-                if(cols[c].has(val)) return false;
-                cols[c].add(val);
+            if(cols[c].has(value)) return false;
+            cols[c].add(value);
 
-                const boxid= Math.floor(r/3)*3 + Math.floor(c/3);
-                if(boxes[boxid].has(val)) return false;
-                boxes[boxid].add(val);
-            }
+            let boxId= Math.floor(r/3)*3+Math.floor(c/3);
+            if(boxes[boxId].has(value)) return false;
+            boxes[boxId].add(value);
+
         }
-        return true;
+    }
+    return true;
 };
