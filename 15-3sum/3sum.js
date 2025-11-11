854a6288bac62463edd244c1b6482c099f5f0e47
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-11 13:53:43 -0800

    Time: 26 ms (98.14%) | Memory: 72.1 MB (71.71%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,20 +3,23 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a,b)=>a-b);
-    let res=[];
+    nums.sort((a,b)=>a-b)
+    let result=[];
+
     for(let i=0;i<nums.length;i++){
-        //if (nums[i] > 0) break;
+        if(nums[i]>0) break;
         if(i>0 && nums[i]===nums[i-1]) continue;
+
         let l=i+1,r=nums.length-1;
         while(l<r){
             let curSum=nums[i]+nums[l]+nums[r];
+            
             if(curSum>0){
                 r--;
             }else if(curSum<0){
                 l++;
             }else{
-                res.push([nums[i],nums[l],nums[r]]);
+                result.push([nums[i],nums[l],nums[r]]);
                 l++;
                 r--;
                 while(l<r && nums[l]===nums[l-1]){
@@ -25,5 +28,5 @@ var threeSum = function(nums) {
             }
         }
     }
-    return res;
+    return result;
 };
