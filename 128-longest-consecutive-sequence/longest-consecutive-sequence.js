6b36228d454fe7a005ad6fd185c7ac3b58fafa33
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-10 16:23:59 -0800

    Time: 53 ms (19.26%) | Memory: 82.8 MB (15.98%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -4,17 +4,22 @@
  */
 var longestConsecutive = function(nums) {
     const numSet = new Set(nums);
-    let longest=0;
-    for(let num of numSet){
-        if(!numSet.has(num-1)){
-            let length=1;
-            let current=num;
-            while(numSet.has(current+1)){
-                length=length+1;
-                current=current+1;
+    let longest = 0;
+
+    for (let n of numSet) {
+        // only start counting if n begins a sequence
+        if (!numSet.has(n - 1)) {
+            let length = 1;
+            let current = n;
+
+            while (numSet.has(current + 1)) {
+                current += 1;
+                length += 1;
             }
-            longest= Math.max(length,longest);
+
+            longest = Math.max(longest, length);
         }
     }
+
     return longest;
 };
