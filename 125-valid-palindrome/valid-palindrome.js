1d107db8fc4d281e3e9a9e96412ac10d108ebc99
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-11 11:14:38 -0800

    Time: 2 ms (93.20%) | Memory: 58 MB (57.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,25 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-
-function alphaNum(c){
-    return (c>='a' && c<='z' ||
-            c>='A' && c<='Z' ||
-            c>='0' && c<='9');
-} 
-
+var isAlphanumeric = function(c) {
+    return ((c>='a'&& c<='z')||(c>='A'&& c<='Z')||(c>='0'&& c<='9'))
+};
+ 
 var isPalindrome = function(s) {
-    let l=0,r=s.length-1;
-
+    let l=0,
+    r=s.length-1;
     while(l<r){
-        while(l<r && !alphaNum(s[l])){
+        while(l<r && !isAlphanumeric(s[l])){
             l++;
         }
-        while(r>l && !alphaNum(s[r])){
+        while(l<r && !isAlphanumeric(s[r])){
             r--;
-        } 
+        }
         if(s[l].toLowerCase()!==s[r].toLowerCase()){
-            return false;
+            return false
         }
         l++;
         r--;
