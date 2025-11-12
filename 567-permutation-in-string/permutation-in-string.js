15544d408f916994f398b4ddaeaf997b734a088a
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-12 11:55:31 -0800

    Time: 28 ms (40.21%) | Memory: 63 MB (23.84%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -5,27 +5,22 @@
  */
 var checkInclusion = function(s1, s2) {
     if(s1.length>s2.length) return false;
-
-    const need={};
+    let need={};
     for(let ch of s1){
-        need[ch]=(need[ch]||0)+1
+        need[ch]=(need[ch] ||0)+1;
     }
-
-    const window={};
+    let window={};
     let l=0;
-
     for(let r=0;r<s2.length;r++){
-        const ch=s2[r];
-        window[ch]=(window[ch] ||0)+1;
+        window[s2[r]]=(window[s2[r]] ||0)+1;
 
-        while(r-l+1>s1.length){
+        if((r-l+1)>s1.length){
             window[s2[l]]--;
-            if(window[s2[l]]===0) delete window[s2[l]]
+            if(window[s2[l]]===0) delete window[s2[l]];
             l++;
         }
-
-        if(r-l+1===s1.length){
-            let match=true;
+        if((r-l+1)===s1.length){
+            let match=true
             for(let key in need){
                 if(need[key]!==window[key]){
                     match=false;
