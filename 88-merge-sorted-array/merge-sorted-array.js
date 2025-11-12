48d0f131c024498118b01e8d5dd54fda268f3d05
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-11 21:10:23 -0800

    Time: 0 ms (100.00%) | Memory: 54.3 MB (82.11%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let t=nums1.length-1;
+    let l1=m-1;
+    let l2=n-1;
+    while (l1 >= 0 && l2 >= 0) {
+        if(nums1[l1]>nums2[l2]){
+            nums1[t]=nums1[l1];
+            l1--;
+        }else{
+            nums1[t]=nums2[l2];
+            l2--;
+        }
+        t--;
+    }
+    while (l2 >= 0) {
+        nums1[t] = nums2[l2];
+        t--;
+        l2--;
+    }
+    return nums1;
+};
