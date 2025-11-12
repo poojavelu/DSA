a4ab8fb042efb5bfacdeecd9a0794d96daf5fa2a
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-12 10:27:29 -0800

    Time: 5 ms (78.46%) | Memory: 56.5 MB (81.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let long=0;
-    let left=0
-    const newSet=new Set();
+    let longest=0,left=0;
+    const newSet= new Set();
     for(let i=0;i<s.length;i++){
         while(newSet.has(s[i])){
             newSet.delete(s[left]);
             left++;
         }
-        newSet.add(s[i]);
-        long=Math.max(long,newSet.size)       
+        newSet.add(s[i])
+        longest=Math.max(longest,newSet.size)
     }
-    return long;
+    return longest;
 };
