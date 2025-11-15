9161b7414e6fcbfdd5ee6bca2ddc78a737cea1d2
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-14 17:06:34 -0800

    Time: 14 ms (91.88%) | Memory: 77.1 MB (48.14%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    const result=new Array(temperatures.length).fill(0);
-    const stack=[];
-    for(let i=0;i<temperatures.length;i++){
-        while(stack.length>0 && temperatures[i]>temperatures[stack[stack.length-1]]){
-            const prevIndex=stack.pop();
-            result[prevIndex]=i-prevIndex;
+        const stack=[];
+        const result= new Array(temperatures.length).fill(0);
+        for(let i=0;i<temperatures.length;i++){
+            while(stack.length>0 && temperatures[i]>temperatures[stack[stack.length-1]]){
+                let prevIndex=stack.pop();
+                result[prevIndex]=i-prevIndex;
+            }
+            stack.push(i)
         }
-        stack.push(i);
-    }
-    return result;
+        return result;
 };
