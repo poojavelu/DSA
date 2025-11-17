3ef48b8c2778b195a4b393c5f24afff84b43e335
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-17 09:58:38 -0800

    Time: 201 ms (51.25%) | Memory: 92.5 MB (27.88%) - LeetSync

--- 883-car-fleet/car-fleet.js
+++ 883-car-fleet/car-fleet.js
@@ -5,16 +5,17 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    let cars=position.map((p,i)=>[p,(target-p)/speed[i]]);
-    cars.sort((a,b)=>b[0]-a[0]);
-    let fleet=0;
-    let time=0;
-    for(let i=0;i<cars.length;i++){
-        let stime=cars[i][1];
-        if(stime>time){
-            fleet++;
-            time=stime;
+    let pair = position.map((p, i) => [p, speed[i]]);
+        pair.sort((a, b) => b[0] - a[0]);
+        let stack = [];
+        for (let [p, s] of pair) {
+            stack.push((target - p) / s);
+            if (
+                stack.length >= 2 &&
+                stack[stack.length - 1] <= stack[stack.length - 2]
+            ) {
+                stack.pop();
+            }
         }
-    }
-    return fleet;
+        return stack.length;
 };
