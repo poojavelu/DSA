d2a8580291980b40cab3f9e7768f7c05bfcff410
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2025-11-18 09:57:05 -0800

    Time: 0 ms (100.00%) | Memory: 57.1 MB (89.54%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-        let left=0;
-        let right=nums.length-1;
-        while(left<=right){
-            let mid=Math.floor((left+right)/2);
-            if(nums[mid]===target) return mid;
-            if(nums[mid]<target){
-                left=mid+1
-            }
-            if(nums[mid]>target){
-                right=mid-1
-            }
+    let left=0,right=nums.length-1;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(nums[mid]<target){
+            left=mid+1
+        }else if(nums[mid]>target){
+            right=mid-1;
+        }else{
+            return mid
         }
-        return -1;
+    }
+    return -1;
 };
