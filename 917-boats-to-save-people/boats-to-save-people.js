bedf2d0d932707949d03c74c2bce2399d358a535
Pooja Veluchamy <pooja.veluchamy@sjsu.edu>
2026-01-06 15:42:28 -0800

    Time: 67 ms (76.77%) | Memory: 64.2 MB (20.65%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.js
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -5,15 +5,15 @@
  */
 var numRescueBoats = function(people, limit) {
     people.sort((a,b)=>a-b);
-        let l=0,r=people.length-1;
-        let minBoat=0;
-        while(l<=r){
-            let sumW=people[l]+people[r];
-            if(sumW<=limit){
-                l++;
-            }
-            r--;
-            minBoat+=1;  
+    let l=0,r=people.length-1;
+    let minBoat=0;
+    while(l<=r){
+        let sumW=people[l]+people[r];
+        if(sumW<=limit){
+            l++;
         }
+        r--;
+        minBoat+=1;  
+    }
     return minBoat
 };
